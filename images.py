baaac517fb05f5cc0355538fc25fa26ea8a2a3f0
joelkothapally <78373501+joelkothapally@users.noreply.github.com>
2021-03-25 01:35:44 +0530

    Update images.py

--- images.py
+++ images.py
@@ -3,6 +3,6 @@ from PIL import Image, ImageFilter
 img = Image.open('./Image_Folder/pikachu.jpg')
 
 filtered_Img = img.filter(ImageFilter.BLUR)
-
+#use png format while using filter
 filtered_Img.save("blur.png", 'png')
 
