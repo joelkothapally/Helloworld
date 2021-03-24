0aa26f08a935038b419dfe0e21d8a7d0d7832f8c
joelkothapally <78373501+joelkothapally@users.noreply.github.com>
2021-03-25 01:45:50 +0530

    Add files via upload
    Convert Images

--- images.py
+++ images.py
@@ -2,7 +2,7 @@ from PIL import Image, ImageFilter
 
 img = Image.open('./Image_Folder/pikachu.jpg')
 
-filtered_Img = img.filter(ImageFilter.BLUR)
-#use png format while using filter
-filtered_Img.save("blur.png", 'png')
+filtered_Img = img.convert('L')
+
+filtered_Img.save("grey.png", 'png')
 
